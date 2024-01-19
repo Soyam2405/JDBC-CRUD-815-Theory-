3e52b00de46c709fa7256af90c871552a00f24cd
hp <hp@DESKTOP-I3D48G7>
2024-01-19 12:05:46 +0530

    commit

--- JDBC_CRUD/src/com/capgemini/Main/palindrome.java
+++ JDBC_CRUD/src/com/capgemini/Main/palindrome.java
@@ -12,7 +12,7 @@ public class palindrome {
 		   n=n/10;    
 		  }    
 		  if(temp==sum)    
-		   System.out.println("palindrome number ");    
+		   System.out.println("palindrome num ");    
 		  else    
 		   System.out.println("not palindrome");    
 		}  
