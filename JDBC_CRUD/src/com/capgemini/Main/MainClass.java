9761efd6e5a4536e91183eae7b57ff3047085a00
hp <hp@DESKTOP-I3D48G7>
2024-01-25 14:39:39 +0530

    commit

--- JDBC_CRUD/src/com/capgemini/Main/MainClass.java
+++ JDBC_CRUD/src/com/capgemini/Main/MainClass.java
@@ -1,10 +1,16 @@
 package com.capgemini.Main;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class MainClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ClassNotFoundException, SQLException
+	{
+		Connection con =null;
    System.out.println("Hello Wrld");
+   Class.forName("com.mysql.cj.jdbc.Driver");
+   con = DriverManager.getConnection("jdbc:mysql://localhost:3306/soyam","","");
+		   System.out.println("Connection Established");
+		   System.out.println(con.getClass().getName());
 	}
 
 }
